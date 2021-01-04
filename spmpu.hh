54ea4bd9acd70395db962432e6457a6a3622bc3d
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2021-01-04 22:16:43 +0900

    little stack

--- spmpu.hh
+++ spmpu.hh
@@ -1,5 +1,8 @@
 #if !defined(_SIMPLE_MPU_)
 
+using std::vector;
+using std::pair;
+using std::make_pair;
 
 template <typename T, int bits> class NInt {
 public:
@@ -106,6 +109,35 @@ template <typename T, int bits> inline void SimplePMPU<T,bits>::nand(const int& 
   return;
 }
 
+template <typename T> class SimpleAddr {
+public:
+  inline SimpleAddr();
+  inline ~SimpleAddr();
+  inline void write(T& dst, const T& src0, const T& src1);
+  inline void next();
+private:
+  vector<pair<T*, T> > q;
+};
+
+template <typename T> inline SimpleAddr<T>::SimpleAddr() {
+  q.resize(sizeof(T) * 8, make_pair(static_cast<T*>(0), T(0)));
+}
+
+template <typename T> inline SimpleAddr<T>::~SimpleAddr() {
+  ;
+}
+
+template <typename T> inline void SimpleAddr<T>::write(T& dst, const T& src0, const T& src1) {
+  q[q.size() - 1] = make_pair(&dst, src0 + src1);
+}
+
+template <typename T> inline void SimpleAddr<T>::next() {
+  if(q[0].first) *(q[0].first) = q[0].second;
+  for(int i = 0; i < q.size() - 1; i ++)
+    q[i] = q[i + 1];
+  q[q.size() - 1] = make_pair(static_cast<T*>(0), T(0));
+}
+
 
 template <typename T, int pages, int ipages> class SimpleMPU {
 public:
@@ -142,31 +174,31 @@ public:
     COND_NOTEQUAL  = 3,
     COND_LESSER    = 4,
     COND_GREATER   = 5
-  } cond_t;
+  } cond_e;
   typedef enum {
-    IOP_NOT,
-    IOP_PLUS,
-    IOP_INC,
-    IOP_DEC,
-    IOP_XOR,
-    IOP_AND,
-    IOP_OR,
-    IOP_SLEFT,
-    IOP_SRIGHT,
-    IOP_CMP,
-    IOP_LDCONTROL,
-    IOP_STCONTROL,
-    IOP_LDIP,
-    IOP_STIP,
-  } int_op_t;
+    IOP_NOT   = 1,
+    IOP_PLUS  = 2,
+    IOP_INC   = 3,
+    IOP_DEC   = 4,
+    IOP_XOR   = 5,
+    IOP_AND   = 6,
+    IOP_OR    = 7,
+    IOP_SLEFT = 8,
+    IOP_SRIGHT = 9,
+    IOP_CMP   = 10,
+    IOP_LDCONTROL = 11,
+    IOP_STCONTROL = 12,
+    IOP_LDIP  = 13,
+    IOP_STIP  = 14,
+  } int_op_e;
   typedef enum {
-    OOP_INTERRUPT,
-    OOP_INTERRUPT_MPU,
-    OOP_IRET,
-    OOP_STPAGE,
-    OOP_STINT,
-    OOP_NOP,
-  } other_op;
+    OOP_INTERRUPT = 1,
+    OOP_INTERRUPT_MPU = 2,
+    OOP_IRET   = 3,
+    OOP_STPAGE = 4,
+    OOP_STINT  = 5,
+    OOP_NOP    = 6,
+  } other_op_e;
   typedef struct {
     uint32_t cond : 6;
     uint32_t type : 1;
@@ -189,37 +221,42 @@ public:
       } other_op;
     } op;
   } mnemonic_t;
+  inline SimpleMPU();
   inline SimpleMPU(const int& npu);
   inline ~SimpleMPU();
-  inline process();
-  std::vector<pu_t> pu;
+  inline void process();
+  vector<pu_t> pu;
+  vector<SimpleAddr<T> > alu;
   int pctr;
 };
 
-template <typename T> inline SimpleMPU::SimpleMPU() {
+template <typename T, int pages, int ipages> inline SimpleMPU<T,pages,ipages>::SimpleMPU() {
   pctr ^= pctr;
 }
 
-template <typename T> inline SimpleMPU::SimpleMPU(const int& npu) {
+template <typename T, int pages, int ipages> inline SimpleMPU<T,pages,ipages>::SimpleMPU(const int& npu) {
   pu.resize(npu);
+  alu.resize(npu);
   pctr ^= pctr;
 }
 
-template <typename T> inline SimpleMPU::~SimpleMPU() {
+template <typename T, int pages, int ipages> inline SimpleMPU<T,pages,ipages>::~SimpleMPU() {
   ;
 }
 
-template <typename T> inline SimpleMPU::process() {
+template <typename T, int pages, int ipages> inline void SimpleMPU<T,pages,ipages>::process() {
   for(int i = 0; i < pu.size(); i ++) {
           auto& p(pu[i]);
-    const auto& mnemonic(*static_cast<const mnemonic_t*>(&p.rip);
+    const auto& mnemonic(*static_cast<const mnemonic_t*>(p.rip));
     const auto  interrupted(p.cond & COND_INTERRUPT);
-    if(mnemonic.cond & cond == mnemonic.cond) {
+    alu[i].next();
+    if((mnemonic.cond & p.cond) == mnemonic.cond) {
       if(mnemonic.type) {
-        const auto& oop(mnemonic.oop.other_op);
+        const auto& oop(mnemonic.op.other_op);
         if(oop.op == OOP_NOP) {
           ;
         } else if(oop.op == OOP_INTERRUPT) {
+          ;
         } else if(interrupted) {
           switch(oop.op) {
           case OOP_INTERRUPT_MPU:
@@ -244,28 +281,29 @@ template <typename T> inline SimpleMPU::process() {
       } else [
         const auto& iop(mnemonic.op.int_op);
         const auto  top(static_cast<T*>(interrupted ? p.ireg : p.reg));
-              T*    rtop;
-        if(ref) {
+              T*    rtop(0);
+        if(iop.ref) {
           // page ref.
           ;
         }
-        const auto& dst(*((ref ? rtop : top) + static_cast<T*>(iop.dst * sizeof(T))));
-        const auto& src(*((ref ? rtop : top) + static_cast<T*>(iop.src * sizeof(T))));
-        const auto& wrt(*((ref ? rtop : top) + static_cast<T*>(iop.wrt * sizeof(T))));
+        const auto& dst(*((iop.ref ? rtop : top) +
+                          static_cast<T*>(iop.dst * sizeof(T))));
+        const auto& src(*((iop.ref ? rtop : top) +
+                          static_cast<T*>(iop.src * sizeof(T))));
+        const auto& wrt(*((iop.ref ? rtop : top) +
+                          static_cast<T*>(iop.wrt * sizeof(T))));
         switch(iop.op) {
         case IOP_NOT:
           wrt = ~ dst;
           break;
         case IOP_PLUS:
-          wrt = dst + src;
+          alu[i].write(wrt, dst, src);
           break;
         case IOP_INC:
-          wrt = dst;
-          ++ wrt;
+          alu[i].write(wrt, dst, 1);
           break;
         case IOP_DEC:
-          wrt = dst;
-          -- wrt;
+          alu[i].write(wrt, dst, - 1);
           break;
         case IOP_XOR:
           wrt = dst ^ src;
@@ -283,7 +321,14 @@ template <typename T> inline SimpleMPU::process() {
           wrt = dst >> src;
           break;
         case IOP_CMP:
-          
+          p.cond &= ~ ((1LL << COND_EQUAL)    |
+                       (1LL << COND_NOTEQUAL) |
+                       (1LL << COND_LESSER)   |
+                       (1LL << COND_GREATER));
+          p.cond |= (dst == src ? (1LL << COND_EQUAL)
+                                : (1LL << COND_NOTEQUAL))    |
+                    (dst <  src ? (1LL << COND_LESSER)  : 0) |
+                    (src <  dst ? (1LL << COND_GREATER) : 0);
           break;
         case IOP_LDCONTROL:
           if(interrupted)
